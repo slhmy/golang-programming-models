6a188e344657a37b41d79df933ec1b6301b387e5
slhmy <1484836413@qq.com>
2022-09-04 15:47:35 +0800

    Optimize the code

--- producer-consumer-control/main.go
+++ producer-consumer-control/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
 
@@ -12,46 +11,56 @@ type Message struct {
 	Content string
 }
 
-func producer(ch chan<- Message, countCh chan int, ctx context.Context) {
+func producer(dataCh chan<- Message, countCh chan int, ctx context.Context) {
+	// Create a unique id for the producer
 	id := rand.Int()
 	for {
 		select {
+		// Handle <-ctx.Done()
 		case <-ctx.Done():
-			break
+			fmt.Printf("Producer_%d quit\n", id)
+			return
 		default:
-			Counter := <-countCh
-			Counter += 1
-			countCh <- Counter
-			ch <- Message{
-				Content: fmt.Sprintf("Producer_%d sending message_%d", id, Counter),
+			// Get message count
+			Count := <-countCh
+			Count += 1
+			countCh <- Count
+
+			// Produce
+			dataCh <- Message{
+				Content: fmt.Sprintf("Producer_%d sending message_%d", id, Count),
 			}
-			time.Sleep(1000 * time.Millisecond)
+			time.Sleep(1 * time.Second)
 		}
 	}
 }
 
-func consumer(ch <-chan Message) {
+func consumer(dataCh <-chan Message) {
+	// Create a unique id for the consumer
 	id := rand.Int()
 	for {
-		d := <-ch
-		fmt.Printf("Consumer_%d get message: %s\n", id, d.Content)
+		// Consume
+		data := <-dataCh
+		fmt.Printf("Consumer_%d get message: %s\n", id, data.Content)
 	}
 }
 
 func main() {
+	// Prepare
 	var ctx, cancel = context.WithCancel(context.Background())
-	dataChannel := make(chan Message, 1)
+	dataChannel := make(chan Message)
 	countChannel := make(chan int, 1)
-	var wg sync.WaitGroup
-	wg.Add(1)
 	countChannel <- 0
+
+	// Start goroutines
 	go producer(dataChannel, countChannel, ctx)
 	go producer(dataChannel, countChannel, ctx)
 	go consumer(dataChannel)
 	go consumer(dataChannel)
-	time.Sleep(10 * time.Second)
+
+	time.Sleep(5 * time.Second)
+
 	fmt.Println("Canceling producer..")
 	cancel()
-	wg.Done()
-	wg.Wait()
+	time.Sleep(1 * time.Second)
 }
